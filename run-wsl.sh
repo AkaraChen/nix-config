7d8c66b1746792a7930867593fb83f55aad1f58c
AkaraChen <akarachen@outlook.com>
2025-07-08 18:42:53 +0800

    chore: remove unused script

--- run-wsl.sh
+++
@@ -1,1 +0,0 @@
-NIX_CONFIG="experimental-features = nix-command flakes" nix run home-manager/master -- switch --flake .#x86_64-linux
--- run.sh
+++
@@ -1,1 +0,0 @@
-NIX_CONFIG="experimental-features = nix-command flakes" nix run home-manager/master -- switch --flake .#linux
