3ea1d219ccb9a7150e0cc146486650e14b6199fc
AkaraChen <akarachen@outlook.com>
2025-07-19 03:51:27 +0800

    feat: hyprland cfg

--- home/home.nix
+++ home/home.nix
@@ -4,4 +4,8 @@
   home.stateVersion = "24.05";
   home.username = "akrc";
   home.homeDirectory = "/home/akrc";
+
+  home.files = {
+    "./.config/hyprland/hyprland.conf".source = "${dotfiles}/.config/hyprland/hyprland.conf";
+  };
 }
