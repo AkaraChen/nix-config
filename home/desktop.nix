de54a4faabebf61f60c585f763b7c00599442420
AkaraChen <akarachen@outlook.com>
2025-07-19 14:44:20 +0800

    build: 在 home/desktop.nix 中添加多个应用程序支持，包括 waybar、kitty、hyprland 和 neovim

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,6 +1,12 @@
 { pkgs, catppuccin, ... }:
 
 {
+    home.packages = with pkgs; [
+        waybar
+        kitty
+        hyprland
+        neovim
+    ];
     wayland.windowManager.hyprland.enable = true;
     catppuccin = {
         enable = true;
