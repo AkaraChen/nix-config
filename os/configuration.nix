b7020bcd330649046bf1a5a759c2f7aaae79a18b
AkaraChen <akarachen@outlook.com>
2025-07-19 12:55:51 +0800

    build: 在 os/configuration.nix 中启用 Hyprland 窗口管理器支持

--- os/configuration.nix
+++ os/configuration.nix
@@ -111,4 +111,8 @@
   # setup gnome for display manager
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
 }
