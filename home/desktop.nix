5f52360e560cf746bfa2617e1c9bf5663b6b0220
AkaraChen <akarachen@outlook.com>
2025-07-19 14:30:52 +0800

    build: 在 home/desktop.nix 中添加 catppuccin 配置，启用多个应用程序支持

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,5 +1,11 @@
 { pkgs, catppuccin, ... }:
 
 {
-    
+    catppuccin = {
+        waybar.enable = true;
+        kitty.enable = true;
+        firefox.enable = true;
+        hyprland.enable = true;
+        neovim.enable = true;
+    };
 }
