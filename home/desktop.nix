d722e54bfffd9b9bd9288be4b8caace50263bef7
AkaraChen <akarachen@outlook.com>
2025-07-19 14:31:41 +0800

    build: 在 home/desktop.nix 中移除 firefox 的启用设置，优化 catppuccin 配置

--- home/desktop.nix
+++ home/desktop.nix
@@ -4,7 +4,6 @@
     catppuccin = {
         waybar.enable = true;
         kitty.enable = true;
-        firefox.enable = true;
         hyprland.enable = true;
         neovim.enable = true;
     };
