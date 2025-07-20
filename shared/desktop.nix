e57398bd8e93cdb509b466b6c0d1a32b884e8d98
AkaraChen <akarachen@outlook.com>
2025-07-20 13:10:04 +0800

    refactor(nix): remove sway, rofi, waybar, and mako configurations

--- shared/desktop.nix
+++ shared/desktop.nix
@@ -4,12 +4,6 @@
     home.packages = with pkgs; [
         nerd-fonts.zed-mono
         ghostty
-        rofi
-        waybar
-        mako
     ];
-    wayland.windowManager.sway.enable = true;
-    home.file = {
-        ".config/sway".source = "${dotfiles}/.config/sway";
-    };
+    
 }
