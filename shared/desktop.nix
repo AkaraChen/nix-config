a5b39c6d1e97383884aad56735ae59a117079593
AkaraChen <akarachen@outlook.com>
2025-07-22 14:30:50 +0800

    refactor: move home.packages

--- shared/desktop.nix
+++ shared/desktop.nix
@@ -1,25 +1,30 @@
 { pkgs, dotfiles, ... }:
 
 {
+    home.packages = with pkgs; [
+
+    ];
     wayland.windowManager.hyprland = {
       enable = true;
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          ", Print, exec, grimblast copy area"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-              let ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
+      settings = {
+        bind =
+          [
+            "$mod, F, exec, firefox"
+            ", Print, exec, grimblast copy area"
+          ]
+          ++ (
+            # workspaces
+            # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+            builtins.concatLists (builtins.genList (i:
+                let ws = i + 1;
+                in [
+                  "$mod, code:1${toString i}, workspace, ${toString ws}"
+                  "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+                ]
+              )
+              9)
+          );
+      }
     };
     home.packages = with pkgs; [
         nerd-fonts.zed-mono
