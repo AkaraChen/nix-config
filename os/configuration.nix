c0e940c94c207fd124a7bdcb3d2ad1ab3593f847
AkaraChen <akarachen@outlook.com>
2025-07-22 15:46:18 +0800

    fix: change default locale to en_US

--- os/configuration.nix
+++ os/configuration.nix
@@ -39,7 +39,7 @@
   time.timeZone = "Asia/Hong_Kong";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   # Configure keymap in X11
   services.xserver.xkb = {
