f3bcf7ba327803ad547800703bb95a47cca9967c
AkaraChen <akarachen@outlook.com>
2025-08-10 10:43:05 +0800

    feat: use nix style ohmyzsh

--- shared/shell.nix
+++ shared/shell.nix
@@ -1,4 +1,4 @@
-{ pkgs, dotfiles, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -16,15 +16,54 @@
     neofetch
   ];
 
+  environment.shellAliases = {
+    "cat" = "bat";
+    "ls" = "lsd";
+    "cd" = "z";
+    "cls" = "clear";
+    "vog" = "gh repo view --web";
+    "his" = "history | grep";
+  };
+
+  environment.extraInit = ''
+    export PATH="$HOME/.local/bin:$PATH"
+  '';
+
+  environment.variables = {
+    NEXT_TELEMETRY_DISABLED = 1;
+    NUXT_TELEMETRY_DISABLED = 1;
+
+    # shell config
+    EDITOR = "nvim";
+
+    # oh-my-zsh config
+    ENABLE_CORRECTION = "true";
+    HYPHEN_INSENSITIVE = "true";
+  };
+
   programs.zsh = {
-    # will conflict with home.file.[".zshrc"]
-    # enable = true;
+    enable = true;
     oh-my-zsh = {
       enable = true;
+      theme = "afowler";
+      plugins = [
+        "git"
+        "golang"
+        "rust"
+        "node"
+        "npm"
+        "python"
+        "brew"
+      ];
     };
+    interactiveShellInit = ''
+      eval "$(fnm env)"
+      eval "$(zoxide init zsh)"
+    '';
   };
-  home.file = {
-    ".zshrc".source = "${dotfiles}/.zshrc";
-    "starship.toml".source = "${dotfiles}/starship.toml";
+
+  programs.starship = {
+    enable = true;
+    settings = { };
   };
 }
