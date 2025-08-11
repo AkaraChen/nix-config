3d130c95eea8596d038d876d0c4e629cada2dd9a
AkaraChen <akarachen@outlook.com>
2025-08-11 21:51:48 +0800

    feat: update stylix to 25.05

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
