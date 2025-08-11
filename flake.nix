55d5f2339f387875b23c9692453ed2f3e12ece30
AkaraChen <akarachen@outlook.com>
2025-08-11 21:58:17 +0800

    feat: update home-manager to 25.05

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # Home Manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # nix darwin
